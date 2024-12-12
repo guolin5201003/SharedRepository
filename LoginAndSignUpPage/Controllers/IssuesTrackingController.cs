df8120ddb449ce5875b53dcc67dde2d63155ca2e
Gray Guo <gguo@TOUCHPOINTMED.COM>
2024-12-12 14:53:32 +0800

    add issue tracking

--- LoginAndSignUpPage/Controllers/IssuesTrackingController.cs
+++ LoginAndSignUpPage/Controllers/IssuesTrackingController.cs
@@ -1,5 +1,6 @@
 ﻿using LoginFormASPCore6.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace LoginFormASPCore6.Controllers
 {
@@ -17,6 +18,18 @@ namespace LoginFormASPCore6.Controllers
         {
             var issues = _context.LocalIssueTracking.ToList();
             return View(issues);
+        }
+
+
+        public IActionResult Download(string fileName)
+        {
+            var fullName = Path.Combine("D:\\OneDrive - TouchPointMed\\6-Work Backup\\2-MyStudying\\1-Repos\\SharedRepository\\LoginAndSignUpPage\\wwwroot", fileName);
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return NotFound(); 
+            }
+            byte[] bytes = Encoding.UTF8.GetBytes(fullName);
+            return File(bytes, "text/plain", fileName.Replace("\\","_")+".txt");
         }
     }
 }
